2fa7516f8e0773ec05b6ec71b1744c332bd0b367
Qikui Li <liqikui@msn.com>
2018-08-25 18:22:20 -0400

    g

--- Vic.SportsStore.WebUI/Controllers/ProductController.cs
+++ Vic.SportsStore.WebUI/Controllers/ProductController.cs
@@ -15,21 +15,25 @@
             this.repository = productRepository;
         }
 
-        public ActionResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository
-                           .Products
-                           .OrderBy(p => p.ProductId)
-                           .Skip((page - 1) * PageSize)
-                            .Take(PageSize),
+            .Products
+            .Where(p => category == null || p.Category == category)
+            .OrderBy(p => p.ProductId)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize),
                 PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                }
+                    TotalItems = category == null
+            ? repository.Products.Count()
+            : repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
