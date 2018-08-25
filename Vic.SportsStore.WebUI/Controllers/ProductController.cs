1dbdb6364e69ab6325e995a993c16e98daeab0f5
Qikui Li <liqikui@msn.com>
2018-08-25 16:24:23 -0400

    gg

--- Vic.SportsStore.WebUI/Controllers/ProductController.cs
+++ Vic.SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,21 +1,27 @@
 ﻿namespace Vic.SportsStore.WebApp.Controllers
 {
     using global::Vic.SportsStore.Domain.Abstract;
+    using System.Linq;
     using System.Web.Mvc;
 
     public class ProductController : Controller
     {
         // GET: Product
         private IProductRepository repository;
-
+        public int PageSize = 3;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ActionResult List()
+        public ActionResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(
+                repository
+                  .Products
+                  .OrderBy(p => p.ProductId)
+                  .Skip((page - 1) * PageSize)
+                  .Take(PageSize));
         }
     }
 }
